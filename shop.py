f7b40b0b1c7dfb77d15ade5bc8075a678f31dc40
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-06-28 22:57:25 +0100

    Add shop and test files

---
+++ shop.py
@@ -0,0 +1,13 @@
+# Simple Shop Program
+# Using exception handling code blocks such as try/ except / else / finally, write a program that simulates a customer in a shop
+# (NB: the more code blocks the better, but try to use at least two key words e.g. try/except, raise)
+# Tasks:
+# Create a dictionary with a minimum of 3 items and prices
+# One of the items needs to cost more than £100
+# Customer’s available money is £100
+# Welcome the customer and display the items and their prices, along with an option to “exit” 
+# Accept the option as an input, an invalid input should raise a ValueError 
+# If the customer can afford it, print out a message saying “Here’s your {item}!” 
+# The user should be then greeted out of the shop, and the program terminated.
+# If the customer cannot afford it, note the attempt and ask if they have more money, if they do and enter the amount it should be added to the balance.
+# The purchase should be tried a maximum of 3 items, if it fails a custom error should be raised and the customer will exit the shop.
---
+++ test_shop.py
@@ -0,0 +1,4 @@
+# Write unit tests for your Simple Shop Program in Task 3.
+# You may need to refactor your function in Task 3 to ‘untangle’ some logic into smaller blocks of code to make it easier to write tests.
+# Write at least 5 unit tests in total covering various and appropriate cases. 
+# An expected test will see your custom error being raised.
